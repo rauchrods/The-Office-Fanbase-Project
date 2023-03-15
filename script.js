0abef849811ed1ee7049538d9cb38c250c991e15
rauch <rauchrodrigues@gmail.com>
2023-03-15 12:09:37 +0530

    yo

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
-function headvidunmute(){
-  let videoelement = document.getElementById("headvideo");
-  if(videoelement.muted==true){
-    videoelement.muted=false;
+function headvidunmute() {
+
+  for (let str of strarr) {
+    let videoelement = document.getElementById(str);
+    if (videoelement.style.display == "block") {
+      if (videoelement.muted == true) {
+        videoelement.muted = false;
+      }
+      else {
+        videoelement.muted = true;
+      }
+    }
+
   }
-  else{
-    videoelement.muted=true;
-  }
-  
+
+
 }
 
 function myFunction() {
-    // Get the text field
-    var copyText = document.getElementById("shareinput");
-  
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-  
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
+  // Get the text field
+  var copyText = document.getElementById("shareinput");
 
-    let sharepopup = document.getElementById("sharepopupmain");
-    sharepopup.style.display="none";
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
 
-    let blurcont = document.getElementById("maincontent");
-    blurcont.style.filter="blur(0)";
-  }
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
 
-  
-  function openshare(){
-    let sharepopup = document.getElementById("sharepopupmain");
-    sharepopup.style.display="flex";
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
 
-    let blurcont = document.getElementById("maincontent");
-    blurcont.style.filter="blur(3px)";
-    
-  }
+  let sharepopup = document.getElementById("sharepopupmain");
+  sharepopup.style.display = "none";
+
+  let blurcont = document.getElementById("maincontent");
+  blurcont.style.filter = "blur(0)";
+}
+
+
+function openshare() {
+  let sharepopup = document.getElementById("sharepopupmain");
+  sharepopup.style.display = "flex";
+
+  let blurcont = document.getElementById("maincontent");
+  blurcont.style.filter = "blur(3px)";
+
+}
+
+let strarr = ["headvideo1", "headvideo2", "headvideo3", "headvideo4", "headvideo5"];
+
+window.onload = function () {
+
+
+
+  let x = parseInt(Math.random() * strarr.length);
+
+  let vidobj = document.getElementById(strarr[x]);
+
+  vidobj.style.display = "block";
+
+}
 
